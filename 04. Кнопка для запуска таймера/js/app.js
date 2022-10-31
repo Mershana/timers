3f6bfa121db3cbfcc4bb4c9eb89bca87be217bf7
Mershana <kriomaas@gmail.com>
2022-10-31 17:57:49 +0300

    04. Кнопка для запуска таймера

---
+++ 04. Кнопка для запуска таймера/js/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// Кнопка для запуска таймера на JavaScript
+/* №1⊗jsPmTmrLB
+Сделайте кнопку, по нажатию на которую в консоль будет выводится обратный отсчет, начиная с 100. */
+/* let start = document.querySelector('#start');
+
+start.addEventListener('click', function(){
+	let i = 100;
+
+	setInterval(function(){
+		console.log(--i);
+	}, 1000)
+}) */
+
+// Многократное нажатие на кнопку
+/* №2⊗jsPmTmrLB
+Возьмите ваше решение предыдущей задачи. Проверьте, что многократное нажатие на кнопку приводит к ускорению отсчета. Исправьте эту проблему. */
+/* let start = document.querySelector('#start');
+
+start.addEventListener('click', function func(){
+	let i = 100;
+
+	setInterval(function(){
+		console.log(--i);
+	}, 1000);
+
+	this.removeEventListener('click', func);
+
+}) ; */
