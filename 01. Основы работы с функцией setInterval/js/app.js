37501fa598de7bdec63f6a2a7cece48a9d6470b8
Mershana <kriomaas@gmail.com>
2022-10-31 17:57:15 +0300

    01. Основы работы с функцией setInterval

---
+++ 01. Основы работы с функцией setInterval/js/app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Основы работы с функцией setInterval в JavaScript
+/* №1⊗jsPmTmrLn
+Запустите таймер, который каждые 3 секунды будет что-нибудь выводить в консоль. */
+/* function timer(){
+	console.log('!');
+}
+
+setInterval(timer, 3000); */
