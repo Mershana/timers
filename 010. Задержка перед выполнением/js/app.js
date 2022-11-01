a541d7f90970436a1a399301d7f3db2db22772cf
Mershana <kriomaas@gmail.com>
2022-11-01 18:01:29 +0300

    010. Задержка перед выполнением

---
+++ 010. Задержка перед выполнением/js/app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// Задержка перед выполнением в JavaScript
+/* №1⊗jsPmTmrDBE
+Дан абзац. Напишите код, который выведет сообщение в этот абзац через 10 секунд после загрузки страницы. */
+/* let elems = document.querySelector('#elem');
+
+window.addEventListener('load', function(){
+	setTimeout(function(){
+		elems.textContent += ('данный текст появился через 10 секунд');
+	}, 10000);
+}); */
+
+// Запуск таймера
+/* №2⊗jsPmTmrDBE
+Выведите в консоль число 0. Через секунду выведите число 1, через две секунды выведите число 2, через 3 секунды выведите число 3. И так далее до бесконечности. */
+
+/* window.addEventListener('load', function(){
+	setTimeout(function(){
+		console.log(0);
+	}, 0);
+});
+
+window.addEventListener('load', function(){
+	setTimeout(function(){
+		console.log(1);
+	}, 1000);
+});
+
+
+window.addEventListener('load', function(){
+	setTimeout(function(){
+		console.log(2);
+	}, 2000);
+});
+
+window.addEventListener('load', function(){
+	setTimeout(function(){
+		console.log(3);
+	}, 3000);
+}); */
